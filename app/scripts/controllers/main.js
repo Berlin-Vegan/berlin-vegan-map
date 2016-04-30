93d1ffb121ddbcb57c142a5716d86370c6e4a02c
Marco Eckstein <marco.eckstein@gmx.de>
2016-04-30 15:14:45 +0200

    Formatting (4 spaces indentation instead of 2)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,194 +15,196 @@ app.controller('MainController', function ($scope, $http, filterFilter) {
     $scope.districts = null;
     
     $http({method: 'GET', url: 'assets/Locations.json'})
-      .success(function(data, status, headers, config) {
-        $scope.locations=data;
-        editLocations();
-        initMap();
-        initDistricts();
-        updateMarkers();
-      })
-      .error(function(data, status, headers, config) {
-      });
+        .success(function(data, status, headers, config) {
+            $scope.locations=data;
+            editLocations();
+            initMap();
+            initDistricts();
+            updateMarkers();
+        })
+        .error(function(data, status, headers, config) {
+        });
 
     $scope.updateMarkers = updateMarkers;
 
     var infoWindow = new google.maps.InfoWindow();
     
     var createMarker = function (location){
-      var marker = new google.maps.Marker({
-        map: $scope.map,
-        position: new google.maps.LatLng(location.latCoord, location.longCoord),
-        title: location.name,
-        location: location
-      });
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(location.latCoord, location.longCoord),
+            title: location.name,
+            location: location
+        });
 
-      google.maps.event.addListener(marker, 'click', function(){
-        infoWindow.setContent('<h2>' + marker.title + '</h2>' + getContent(marker.location));
-        infoWindow.open($scope.map, marker);
-      });
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + getContent(marker.location));
+            infoWindow.open($scope.map, marker);
+        });
 
-      $scope.markers.push(marker);
+        $scope.markers.push(marker);
     }
     
     function editLocations() {
     
-      for (var i = 0; i < $scope.locations.length; i++) {
-      
-        $scope.locations[i].commentWithoutFormatting = removeFormatting($scope.locations[i].comment);
+        for (var i = 0; i < $scope.locations.length; i++) {
         
-        $scope.locations[i].getOpeningTimeToday = function() { 
-        
-          var currentWeekDay = new Date().getDay();
-          var otString;
-          
-          switch (currentWeekDay) {
-            case 0:
-              otString = this.otSun;
-              break;
-            case 1:
-              otString = this.otMon;
-              break;
-            case 2:
-              otString = this.otTue;
-              break;
-            case 3:
-              otString = this.otWed;
-              break;
-            case 4:
-              otString = this.otThu;
-              break;
-            case 5:
-              otString = this.otFri;
-              break;
-            case 6:
-              otString = this.otSat;
-              break;
-          }
-          
-          return otString;
+            $scope.locations[i].commentWithoutFormatting = removeFormatting($scope.locations[i].comment);
+            
+            $scope.locations[i].getOpeningTimeToday = function() { 
+            
+                var currentWeekDay = new Date().getDay();
+                var otString;
+                
+                switch (currentWeekDay) {
+                    case 0:
+                        otString = this.otSun;
+                        break;
+                    case 1:
+                        otString = this.otMon;
+                        break;
+                    case 2:
+                        otString = this.otTue;
+                        break;
+                    case 3:
+                        otString = this.otWed;
+                        break;
+                    case 4:
+                        otString = this.otThu;
+                        break;
+                    case 5:
+                        otString = this.otFri;
+                        break;
+                    case 6:
+                        otString = this.otSat;
+                        break;
+                }
+                
+                return otString;
+            };
         };
-      };
     }
     
     function removeFormatting(locationComment) {
-      return locationComment.replace(/&shy;/g, "").replace(/<br\/>/g, " ");
+        return locationComment.replace(/&shy;/g, "").replace(/<br\/>/g, " ");
     }
     
     function initMap() {
     
-      var mapOptions = {
-        zoom: 12,
-        center: new google.maps.LatLng(52.5200070,13.4049540),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
+        var mapOptions = {
+            zoom: 12,
+            center: new google.maps.LatLng(52.5200070,13.4049540),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
 
-      $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      $scope.markers = [];
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        $scope.markers = [];
 
-      for (var i = 0; i < $scope.locations.length; i++){
-        createMarker($scope.locations[i]);
-      }
+        for (var i = 0; i < $scope.locations.length; i++){
+            createMarker($scope.locations[i]);
+        }
 
-      $scope.openInfoWindow = function(e, selectedMarker){
-        e.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
-      }
+        $scope.openInfoWindow = function(e, selectedMarker){
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+        }
     }
     
     function updateMarkers() {
-      $scope.filteredMarkers = getFilteredMarkers();
-      
-      for (var i = 0; i < $scope.markers.length; i++){
-        var marker = $scope.markers[i];
+    
+        $scope.filteredMarkers = getFilteredMarkers();
         
-        if ($scope.filteredMarkers.indexOf(marker) >= 0) {
-          marker.setMap($scope.map);
-        } else {
-          marker.setMap(null);
+        for (var i = 0; i < $scope.markers.length; i++) {
+        
+            var marker = $scope.markers[i];
+            
+            if ($scope.filteredMarkers.indexOf(marker) >= 0) {
+                marker.setMap($scope.map);
+            } else {
+                marker.setMap(null);
+            }
         }
-      }
     }
     
     function getFilteredMarkers() {
 
-      var locationPattern = {};
- 
-      if ($scope.search.textAppliesToAllFields) {
-        locationPattern.$ = $scope.search.text;
-      } else {
-        locationPattern.name = $scope.search.text;
-      }
-      
-      if ($scope.search.district !== allDistricts) {
-        locationPattern.district = $scope.search.district;
-      }
-      
-      if ($scope.search.completelyVegan) {
-        locationPattern.vegan = "5"; // TODO: Check if equivalent to legacy app's "not 2 and not 4".
-      }
-      
-      if ($scope.search.organic) {
-        locationPattern.organic = "1";
-      }
-      
-      if ($scope.search.glutenFree) {
-        locationPattern.glutenFree = "1";
-      }
-      
-      if ($scope.search.handicappedAccessible) {
-        locationPattern.handicappedAccessible = "1";
-      }
-      
-      if ($scope.search.handicappedAccessibleWc) {
-        locationPattern.handicappedAccessibleWc = "1";
-      }
-      
-      if ($scope.search.organic) {
-        locationPattern.organic = "1";
-      }
-      
-      if ($scope.search.organic) {
-        locationPattern.organic = "1";
-      }
-      
-      if ($scope.search.dog) {
-        locationPattern.dog = "1";
-      }
-      
-      if ($scope.search.wlan) {
-        locationPattern.wlan = "1";
-      }
-      
-      if ($scope.search.catering) {
-        locationPattern.catering = "1";
-      }
-      
-      if ($scope.search.delivery) {
-        locationPattern.delivery = "1";
-      }
-      
-      return filterFilter($scope.markers, { location: locationPattern });
+        var locationPattern = {};
+   
+        if ($scope.search.textAppliesToAllFields) {
+            locationPattern.$ = $scope.search.text;
+        } else {
+            locationPattern.name = $scope.search.text;
+        }
+        
+        if ($scope.search.district !== allDistricts) {
+            locationPattern.district = $scope.search.district;
+        }
+        
+        if ($scope.search.completelyVegan) {
+            locationPattern.vegan = "5"; // TODO: Check if equivalent to legacy app's "not 2 and not 4".
+        }
+        
+        if ($scope.search.organic) {
+            locationPattern.organic = "1";
+        }
+        
+        if ($scope.search.glutenFree) {
+            locationPattern.glutenFree = "1";
+        }
+        
+        if ($scope.search.handicappedAccessible) {
+            locationPattern.handicappedAccessible = "1";
+        }
+        
+        if ($scope.search.handicappedAccessibleWc) {
+            locationPattern.handicappedAccessibleWc = "1";
+        }
+        
+        if ($scope.search.organic) {
+            locationPattern.organic = "1";
+        }
+        
+        if ($scope.search.organic) {
+            locationPattern.organic = "1";
+        }
+        
+        if ($scope.search.dog) {
+            locationPattern.dog = "1";
+        }
+        
+        if ($scope.search.wlan) {
+            locationPattern.wlan = "1";
+        }
+        
+        if ($scope.search.catering) {
+            locationPattern.catering = "1";
+        }
+        
+        if ($scope.search.delivery) {
+            locationPattern.delivery = "1";
+        }
+        
+        return filterFilter($scope.markers, { location: locationPattern });
     }
     
     function initDistricts() {
     
-      $scope.districts = [];
-      var districtSet = {};
-      
-      for (var i = 0; i < $scope.locations.length; i++) {
-        districtSet[$scope.locations[i].district] = "";
-      }
-      
-      for (var district in districtSet) {
-        $scope.districts.push(district);
-      }
-      
-      $scope.districts.sort();
-      $scope.districts.unshift(allDistricts);
+        $scope.districts = [];
+        var districtSet = {};
+        
+        for (var i = 0; i < $scope.locations.length; i++) {
+            districtSet[$scope.locations[i].district] = "";
+        }
+        
+        for (var district in districtSet) {
+            $scope.districts.push(district);
+        }
+        
+        $scope.districts.sort();
+        $scope.districts.unshift(allDistricts);
     }
     
     function getContent(location) {
-      return '<div class="infoWindowContent">' + location.comment + '</div>';
+        return '<div class="infoWindowContent">' + location.comment + '</div>';
     }
-  });
+});
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,22 +2,22 @@
 
 describe('Controller: MainController', function () {
 
-  // load the controller's module
-  beforeEach(module('berlinVeganMapApp'));
+    // load the controller's module
+    beforeEach(module('berlinVeganMapApp'));
 
-  var mainCtrl;
-  var scope;
+    var mainCtrl;
+    var scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    mainCtrl = $controller('MainController', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        mainCtrl = $controller('MainController', {
+            $scope: scope
+        });
+    }));
+
+    it('should have no locations to start', function () {
+        expect(scope.locations.length).toBe(0);
     });
-  }));
-
-  it('should have no locations to start', function () {
-    expect(scope.locations.length).toBe(0);
-  });
 
 });
