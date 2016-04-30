a5e756e74cac26413a94056893e1f9f387e56877
Marco Eckstein <marco.eckstein@gmx.de>
2016-04-30 14:46:53 +0200

    Added simplistic method getOpeningTimeToday()

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -47,9 +47,43 @@ angular.module('berlinVeganMapApp')
     }
     
     function editLocations() {
-      for (var i = 0; i < $scope.locations.length; i++){
+    
+      for (var i = 0; i < $scope.locations.length; i++) {
+      
         $scope.locations[i].commentWithoutFormatting = removeFormatting($scope.locations[i].comment);
-      }
+        
+        $scope.locations[i].getOpeningTimeToday = function() { 
+        
+          var currentWeekDay = new Date().getDay();
+          var otString;
+          
+          switch (currentWeekDay) {
+            case 0:
+              otString = this.otSun;
+              break;
+            case 1:
+              otString = this.otMon;
+              break;
+            case 2:
+              otString = this.otTue;
+              break;
+            case 3:
+              otString = this.otWed;
+              break;
+            case 4:
+              otString = this.otThu;
+              break;
+            case 5:
+              otString = this.otFri;
+              break;
+            case 6:
+              otString = this.otSat;
+              break;
+          }
+          
+          return otString;
+        };
+      };
     }
     
     function removeFormatting(locationComment) {
