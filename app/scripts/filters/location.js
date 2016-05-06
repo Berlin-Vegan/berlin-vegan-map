e8fe4fc84b84d085f040aa215804c2ef8c181823
Marco Eckstein <marco.eckstein@gmx.de>
2016-05-06 22:23:13 +0200

    Refactoring: Move distance calculation to UtilService

--- app/scripts/filters/location.js
+++ app/scripts/filters/location.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.filter('location', function(filterFilter) {
+app.filter('location', function(filterFilter, UtilService) {
 
     return function(locations, query) {
 
@@ -72,7 +72,7 @@ app.filter('location', function(filterFilter) {
         var filterFunction2 = function(location, index, array) {
 
             if (query.distance.enabled) {            
-                return getDistanceFromLatLonInKm(
+                return UtilService.getDistanceFromLatLonInKm(
                     query.distance.position.lat(), 
                     query.distance.position.lng(), 
                     parseFloat(location.latCoord), 
@@ -88,24 +88,4 @@ app.filter('location', function(filterFilter) {
         filteredLocations = filterFilter(filteredLocations, filterFunction2); 
         return filteredLocations;
     };
-    
-    // From http://stackoverflow.com/q/18883601/443836
-    function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat2-lat1);  // deg2rad below
-        var dLon = deg2rad(lon2-lon1); 
-        var a = 
-            Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-            Math.sin(dLon/2) * Math.sin(dLon/2)
-            ; 
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        var d = R * c; // Distance in km
-        return d;
-    }
-
-    function deg2rad(deg) {
-        return deg * (Math.PI/180)
-    }
 });
