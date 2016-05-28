fec0ca4e88e0f2d2c0dc8e4856cf9c7dda439856
Marco Eckstein <marco.eckstein@gmx.de>
2016-05-28 14:25:20 +0200

    Refactoring (minor)

--- app/scripts/services/info_window_view.js
+++ app/scripts/services/info_window_view.js
@@ -4,16 +4,16 @@
  * There have been great technical diffculties when trying to define the google.maps.InfoWindow content HTML 
  * with a proper AngularJS template. So, to at least have it in a dedicated file, we use this service.
  */
-app.factory('InfoWindowViewService', function() {
+app.factory("InfoWindowViewService", function() {
 
     var service = {};
     
     service.getContent = function(location) {
-        return '<h2>' + location.name + '</h2>' 
-            + '<div class="infoWindowContent">' 
-            + '<p>' + location.tags.join(", ") + '</p>'
-            + '<p>' + location.comment + '</p>'
-            + '</div>';
+        return "<h2>" + location.name + "</h2>" 
+            + "<div class='infoWindowContent'>" 
+            + "<p>" + location.tags.join(", ") + "</p>"
+            + "<p>" + location.comment + "</p>"
+            + "</div>";
     };
     
     return {
