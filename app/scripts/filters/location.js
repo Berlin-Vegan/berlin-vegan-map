1d104baed447bbde4c5025922a8bf47fdf4ef19c
Marco Eckstein <marco.eckstein@gmx.de>
2016-05-29 20:11:24 +0200

    Improved text normalization

--- app/scripts/filters/location.js
+++ app/scripts/filters/location.js
@@ -151,7 +151,7 @@ app.filter('location', function(filterFilter) {
         
         // So we find café when searching for cafe and vice versa.
         // Analogous motivation for the other letters.
-        text = text.replace(/é/g, "e").replace(/á/g, "a").replace(/à/g, "a").replace(/ñ/g, "n");
+        text = text.replace(/é/g, "e").replace(/á/g, "a").replace(/à/g, "a").replace(/â/g, "a").replace(/ñ/g, "n");
         
         return text.toLowerCase();
     }
