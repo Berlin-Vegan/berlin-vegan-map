443fa13a24b7800bfb5a3894f37f9fb89bbcb1dd
Marco Eckstein <marco.eckstein@gmx.de>
2016-06-05 20:04:40 +0200

    Copy assets when creating dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -308,7 +308,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/{,*/}*.*'
+            'fonts/{,*/}*.*',
+            'assets/{,*/}*.*'
           ]
         }, {
           expand: true,
