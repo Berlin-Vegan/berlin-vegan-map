8ce3ed9073614099c011cc973568fa00b6fab015
Marco Eckstein <marco.eckstein@gmx.de>
2017-05-29 04:53:08 +0200

    Repairing app for deployment build

--- app/scripts/appendAppToDom.js
+++
@@ -1,8 +0,0 @@
-document.body.setAttribute("ng-app", "berlinVeganMapApp");
-
-var innerDiv = document.createElement('div');
-innerDiv.setAttribute("ng-view", "");
-var outerDiv = document.createElement('div');
-outerDiv.setAttribute("class", "container-fluid");
-outerDiv.appendChild(innerDiv);
-document.body.appendChild(outerDiv);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,22 @@
  * # MainController
  * Controller of the berlinVeganMapApp
  */
-app.controller('MainController', function ($scope, $http, $timeout, LocationLogicService, InfoWindowViewService, I18nService, filterFilter, locationFilter, kilometerFilter) {
+app.controller('MainController', function (
+    $scope, 
+    $http, 
+    $timeout, 
+    $window, 
+    LocationLogicService, 
+    InfoWindowViewService, 
+    I18nService, 
+    filterFilter, 
+    locationFilter, 
+    kilometerFilter
+) {
+
+    if (typeof(google) === "undefined") {
+        $window.location.reload(); // Dirty hack TODO
+    }
 
     var debugMode = false; // TODO: Set something like that depending on build.
     var locationsUrl = (debugMode ? "assets/" : "/app/data/") + "GastroLocations.json";
