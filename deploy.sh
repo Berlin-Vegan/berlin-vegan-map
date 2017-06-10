9d0974f86b2de3b71492c7757265bd2591a05dfe
Marco Eckstein <marco.eckstein@gmx.de>
2017-06-10 23:28:08 +0200

    Add Windows help to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ bower install
 grunt --force
 rm dist/.htaccess
 rsync -avz dist/ deploy@berlin-vegan.de:/var/www/berlin-vegan-wp/map/
+# On Windows, you may use this instead of rsync (rm -rf * in map/ on server before):
+# scp -r dist/* deploy@berlin-vegan.de:/var/www/berlin-vegan-wp/map/
