0b761febe3c3c5e2d56de2c14261f1808c7d630f
Marco Eckstein <marco.eckstein@gmx.de>
2017-10-07 12:54:05 +0200

    Refactoring

--- app/scripts/services/location_logic.js
+++ app/scripts/services/location_logic.js
@@ -1,12 +1,11 @@
 "use strict";
 
-app.factory('LocationLogicService', function(I18nService) {
+app.factory('LocationLogicService', function(OpeningTimesService) {
 
     var veganCategories = [];
     veganCategories[5] = "vegan";
     veganCategories[4] = "vegetarian";
     veganCategories[2] = "omnivorous";
-    var i18n = I18nService.getI18n();
     var jsCommon = new JsCommon();
     var service = {};
     
@@ -28,67 +27,16 @@ app.factory('LocationLogicService', function(I18nService) {
         }
 
         location.address = location.street + ", " + location.cityCode + " " + location.district;
-
-        location.openingTimes = [
-            new OpeningTime(0, location.otSun), 
-            new OpeningTime(1, location.otMon), 
-            new OpeningTime(2, location.otTue), 
-            new OpeningTime(3, location.otWed), 
-            new OpeningTime(4, location.otThu), 
-            new OpeningTime(5, location.otFri), 
-            new OpeningTime(6, location.otSat)
-        ];
+        location.openingTimes = OpeningTimesService.getOpeningTimes(location);
+        location.getOpeningTimeTodayFriendly = 
+            function() { return OpeningTimesService.getOpeningTimeTodayFriendly(location.openingTimes); };
+        location.getOpeningTimesCompressed = 
+            function() { return OpeningTimesService.getOpeningTimesCompressed(location.openingTimes) };
+        location.isOpen = 
+            function(weekDay, timeAsDate) { return OpeningTimesService.isOpen(this.openingTimes, weekDay, timeAsDate) };
+        location.getOpenComment = 
+            function(language) { return OpeningTimesService.getOpenComment(location, language); };
         
-        location.getOpeningTimeTodayFriendly = function() {
-        
-            var otIntervalToday = this.openingTimes[new Date().getDay()].interval;
-            
-            if (otIntervalToday.isOpen) {
-                return i18n.openingTimes.isOpenToday + ": " + otIntervalToday.friendly;
-            } else {
-                return i18n.openingTimes.isClosedToday;
-            }
-        }
-        
-        location.isOpen = function(weekDay, timeAsDate) {
-            return jsCommon.openingTimesUtil.isOpen(
-                this.openingTimes.map(function(it) { return it.interval.timeInterval }), 
-                weekDay, 
-                timeAsDate
-            );
-        }
-
-        location.getOpenComment = function(language) {
-            return language === "en" ? 
-                "Please see location website for opening time details!" :  this.openComment;
-        };
-        
-        location.getOpeningTimesCompressed = function() {
-        
-            var compressedOts = [];
-            
-            for (var day = 1; day <= 7; day++) {
-            
-                var ot = this.openingTimes[day === 7 ? 0 : day];
-                
-                if (day > 1) {
-                
-                    var lastGroup = compressedOts[compressedOts.length - 1];
-                    
-                    if (lastGroup[0].interval.friendly === ot.interval.friendly) {
-                        lastGroup.push(ot);
-                        continue;
-                    }
-                }
-                
-                var group = [];
-                group.push(ot);
-                compressedOts.push(group);
-            }
-            
-            return compressedOts;
-        }
-
         location.position = {
             lat: function() { return location.latCoord; },
             lng: function() { return location.longCoord; }
@@ -109,53 +57,6 @@ app.factory('LocationLogicService', function(I18nService) {
     
     function removeFormatting(locationComment) {
         return locationComment ? locationComment.replace(/&shy;/g, "").replace(/<br\/>/g, " ") : locationComment;
-    }
-    
-    function OpeningTime(dayIndex, otString) {
-        this.dayIndex = dayIndex;
-        this.friendlyDay = i18n.enums.weekday[dayIndex + ""];
-        this.friendlyDayShort = I18nService.abbreviateWeekDay(this.friendlyDay);
-        this.interval = parseOpeningTimeInterval(otString);
-    }
-    
-    function parseOpeningTimeInterval(otString) {
-        var timeInterval;
-        var friendly;
-
-        if (!!otString) {
-            timeInterval = parseTimeInterval(otString);
-            friendly = I18nService.formatTimeInterval(timeInterval.begin, timeInterval.end); // TODO
-        } else {
-            timeInterval = null;
-            friendly = i18n.openingTimes.isClosed;
-        }
-
-        return new OpeningTimeInterval(timeInterval, friendly);
-    }
-
-    function OpeningTimeInterval(timeInterval, friendlyString) {
-        this.timeInterval = timeInterval;
-        this.friendly = friendlyString;
-        this.isOpen = function() { return !!timeInterval };
-    }
-
-    function parseTimeInterval(intervalString) {
-        var parts = intervalString.split(" - ");
-        var begin = parseTimeAsDate(parts[0])
-        var end = parseTimeAsDate(parts[1])
-        return new TimeInterval(begin, end);
-
-        function parseTimeAsDate(timeString) {
-            var time = jsCommon.dateUtil.parseTime(timeString);
-            var date = new Date(0);
-            date.setHours(time.hours, time.minutes);
-            return date;
-        }
-    }
-
-    function TimeInterval(beginTimeAsDate, endTimeAsDate) {
-        this.begin = beginTimeAsDate;
-        this.end = endTimeAsDate;
     }
     
     service.getSortedTags = function() {
---
+++ app/scripts/services/opening_times.js
@@ -0,0 +1,137 @@
+"use strict";
+
+app.factory('OpeningTimesService', function(I18nService) {
+
+    var veganCategories = [];
+    veganCategories[5] = "vegan";
+    veganCategories[4] = "vegetarian";
+    veganCategories[2] = "omnivorous";
+    var i18n = I18nService.getI18n();
+    var jsCommon = new JsCommon();
+    var service = {};
+    
+    service.getOpeningTimes = function(location) {
+        return [
+            new OpeningTime(0, location.otSun), 
+            new OpeningTime(1, location.otMon), 
+            new OpeningTime(2, location.otTue), 
+            new OpeningTime(3, location.otWed), 
+            new OpeningTime(4, location.otThu), 
+            new OpeningTime(5, location.otFri), 
+            new OpeningTime(6, location.otSat)
+        ];
+    };
+
+    service.getOpenComment= function(location, language) {
+        return language === "en" ? 
+            "Please see location website for opening time details!"
+            :
+            location.openComment;
+    };
+
+    service.getOpeningTimeTodayFriendly = function(openingTimes) {
+        var otIntervalToday = openingTimes[new Date().getDay()].interval;
+        return otIntervalToday.isOpen ? 
+            i18n.openingTimes.isOpenToday + ": " + otIntervalToday.friendly
+            :
+            i18n.openingTimes.isClosedToday;
+    };
+
+    service.getOpeningTimesCompressed = function(openingTimes) {
+        var compressedOts = [];
+        
+        for (var day = 1; day <= 7; day++) {
+        
+            var ot = openingTimes[day === 7 ? 0 : day];
+            
+            if (day > 1) {
+            
+                var lastGroup = compressedOts[compressedOts.length - 1];
+                
+                if (lastGroup[0].interval.friendly === ot.interval.friendly) {
+                    lastGroup.push(ot);
+                    continue;
+                }
+            }
+            
+            var group = [];
+            group.push(ot);
+            compressedOts.push(group);
+        }
+        
+        return compressedOts;
+    };
+    
+    service.isOpen = function(openingTimes, weekDay, timeAsDate) {
+        return jsCommon.openingTimesUtil.isOpen(
+            openingTimes.map(function(it) { return it.interval.timeInterval }), 
+            weekDay, 
+            timeAsDate
+        );
+    };
+
+    function OpeningTime(dayIndex, otString) {
+        this.dayIndex = dayIndex;
+        this.friendlyDay = i18n.enums.weekday[dayIndex + ""];
+        this.friendlyDayShort = I18nService.abbreviateWeekDay(this.friendlyDay);
+        this.interval = parseOpeningTimeInterval(otString);
+    }
+    
+    function parseOpeningTimeInterval(otString) {
+        var timeInterval;
+        var friendly;
+
+        if (!!otString) {
+            timeInterval = parseTimeInterval(otString);
+            friendly = I18nService.formatTimeInterval(timeInterval.begin, timeInterval.end); // TODO
+        } else {
+            timeInterval = null;
+            friendly = i18n.openingTimes.isClosed;
+        }
+
+        return new OpeningTimeInterval(timeInterval, friendly);
+    }
+
+    function OpeningTimeInterval(timeInterval, friendlyString) {
+        this.timeInterval = timeInterval;
+        this.friendly = friendlyString;
+        this.isOpen = function() { return !!timeInterval };
+    }
+
+    function parseTimeInterval(intervalString) {
+        var parts = intervalString.split(" - ");
+        var begin = parseTimeAsDate(parts[0])
+        var end = parseTimeAsDate(parts[1])
+        return new TimeInterval(begin, end);
+
+        function parseTimeAsDate(timeString) {
+            var time = jsCommon.dateUtil.parseTime(timeString);
+            var date = new Date(0);
+            date.setHours(time.hours, time.minutes);
+            return date;
+        }
+    }
+
+    function TimeInterval(beginTimeAsDate, endTimeAsDate) {
+        this.begin = beginTimeAsDate;
+        this.end = endTimeAsDate;
+    }
+    
+    return {
+        getOpeningTimes: function(location) {
+            return service.getOpeningTimes(location);
+        },
+        getOpenComment: function(location, language) {
+            return service.getOpenComment(location, language);
+        },
+        getOpeningTimeTodayFriendly: function(openingTimes) {
+            return service.getOpeningTimeTodayFriendly(openingTimes);
+        },
+        getOpeningTimesCompressed: function(openingTimes) {
+            return service.getOpeningTimesCompressed(openingTimes);
+        },
+        isOpen: function(openingTimes, weekDay, timeAsDate) {
+            return service.isOpen(openingTimes, weekDay, timeAsDate);
+        },
+    };
+});
