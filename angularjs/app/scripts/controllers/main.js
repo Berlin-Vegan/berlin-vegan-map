Berlin-Vegan/berlin-vegan-map

commit 7566947456d3fc1079166192a03255061684de3f

Refactoring

angularjs/app/scripts/controllers/main.js
@@ -140,14 +140,8 @@ app.controller('MainController', function (
         var locationMarkers = getLocationMarkers();
 
         for (var i = 0; i < locationMarkers.length; i++) {
-
-            var marker = locationMarkers[i];
-
-            if ($scope.filteredLocationMarkers.indexOf(marker) >= 0) {
-                marker.setMap($scope.map);
-            } else {
-                marker.setMap(null);
-            }
+            var map = $scope.filteredLocationMarkers.indexOf(marker) >= 0 ? $scope.map : null;
+            locationMarkers[i].setMap(map);
         }
     }
 

commit 2dac3e7c02a8983fbc3b0a25eda2bc8840998410

Bug fix
I broke this in commit 7566947456d3fc1079166192a03255061684de3f.

angularjs/app/scripts/controllers/main.js
@@ -139,8 +139,9 @@ app.controller('MainController', function (
         var locationMarkers = getLocationMarkers();
 
         for (var i = 0; i < locationMarkers.length; i++) {
+            var marker = locationMarkers[i];
             var map = $scope.filteredLocationMarkers.indexOf(marker) >= 0 ? $scope.map : null;
-            locationMarkers[i].setMap(map);
+            marker.setMap(map);
         }
     }
 