8300119f9148484bd201d04fa531a1ee3c9d9bb7
Marco Eckstein <marco.eckstein@gmx.de>
2018-05-06 14:43:41 +0200

    Bug fix

--- angular/src/app/model/opening-time-interval.ts
+++ angular/src/app/model/opening-time-interval.ts
@@ -2,7 +2,7 @@ import { TimeInterval } from "./time-interval";
 
 export class OpeningTimeInterval {
 
-    constructor(public readonly timeInterval: TimeInterval, public readonly friendly: string) {}
+    constructor(public readonly timeInterval: TimeInterval | null, public readonly friendly: string) {}
 
     isOpen(): boolean {
         return !!this.timeInterval;
