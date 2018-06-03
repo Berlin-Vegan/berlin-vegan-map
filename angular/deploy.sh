2e0717b4d448cfd05fefc914909fe1ac07c4045f
Marco Eckstein <marco.eckstein@gmx.de>
2018-06-03 17:41:17 +0200

    Bug fix

--- angular/deploy.sh
+++ angular/deploy.sh
@@ -7,18 +7,22 @@ else
 fi
 
 if [ $MODE = "production" ]; then
-    BASE_HREF="map/"
+    BASE_HREF="/map/"
 elif [ $MODE = "staging" ]; then
-    BASE_HREF="map2/"
+    BASE_HREF="/map2/"
 else
     echo "Illegal argument"
     exit -1
 fi
 
-npm run build -- --base-href $BASE_HREF
-rm dist/assets/*.json # Only needed for development
+# Setting MSYS2_ARG_CONV_EXCL is necessary for Git Bash (on Windows).
+# See https://stackoverflow.com/questions/50662064/
+MSYS2_ARG_CONV_EXCL="--base-href" npm run build -- --base-href=$BASE_HREF
 
-DEPLOY_DIR="/var/www/berlin-vegan-wp/"$BASE_HREF
+# The .json files are only needed for development.
+rm dist/assets/*.json
+
+DEPLOY_DIR="/var/www/berlin-vegan-wp"$BASE_HREF
 rsync -avz dist/ deploy@berlin-vegan.de:$DEPLOY_DIR
 # On Windows, you may use this instead of rsync (rm -rf * in $DEPLOY_DIR on server before):
 # scp -r dist/* deploy@berlin-vegan.de:$DEPLOY_DIR
