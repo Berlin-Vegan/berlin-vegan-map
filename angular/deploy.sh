27fc37b550a2b56b9324ca26f06d6fc744c52001
Sandy Meier <smeier@tapnic.com>
2018-06-10 17:05:48 +0200

    added angular2 redirect rules https://angular.io/guide/deployment#production-servers

--- angular/deploy.sh
+++ angular/deploy.sh
@@ -21,6 +21,7 @@ MSYS2_ARG_CONV_EXCL="--base-href" npm run build -- --base-href=$BASE_HREF
 
 # The .json files are only needed for development.
 rm dist/assets/*.json
+mv dist/assets/htaccess dist/.htaccess
 
 DEPLOY_DIR="/var/www/berlin-vegan-wp"$BASE_HREF
 
