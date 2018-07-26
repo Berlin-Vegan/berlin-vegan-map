eed860c0e3b9a91803c894847ecb5371c87af15a
Marco Eckstein <marco.eckstein@gmx.de>
2018-07-26 21:26:26 +0200

    Configure swiping

--- angular/src/app/hammer-provider.ts
+++ angular/src/app/hammer-provider.ts
@@ -7,8 +7,8 @@ delete Hammer.defaults.cssProps.userSelect; // Re-enable text selection
 export class HammerConfig extends HammerGestureConfig  {
     overrides = {
         "swipe": {
-            velocity: 0.3, // Default: 0.3
-            threshold: 10, // Default: 10
+            velocity: 0.7, // Default: 0.3
+            threshold: 90, // Default: 10
         }
     };
 
