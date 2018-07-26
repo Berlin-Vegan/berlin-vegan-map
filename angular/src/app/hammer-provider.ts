6d5c55316c4ef3f4608a3ea90cfe9fef48b67b3b
Marco Eckstein <marco.eckstein@gmx.de>
2018-07-26 21:07:44 +0200

    Bug fix: Scrolling on mobile devices possible again

--- angular/src/app/hammer-provider.ts
+++ angular/src/app/hammer-provider.ts
@@ -2,7 +2,8 @@ import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from "@angular/platform-br
 import "hammerjs";
 import "hammer-timejs";
 
-delete (window as any).Hammer.defaults.cssProps.userSelect; // Re-enable text selection
+declare var Hammer: any;
+delete Hammer.defaults.cssProps.userSelect; // Re-enable text selection
 
 export class HammerConfig extends HammerGestureConfig  {
     overrides = {
@@ -11,6 +12,13 @@ export class HammerConfig extends HammerGestureConfig  {
             threshold: 10, // Default: 10
         }
     };
+
+    buildHammer(element: HTMLElement) {
+        const hammer = new Hammer(element, {
+            touchAction: "pan-y"
+        });
+        return hammer;
+    }
 }
 
 export const HammerProvider = { provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig };
