Berlin-Vegan/berlin-vegan-map

commit 951eeb9ece83840f093c7da64b82b5d8057ce460

New feature: Optionally center location when clicking in list

angular/src/app/google-map/google-map.component.ts
@@ -137,9 +137,13 @@ export class GoogleMapComponent implements OnInit {
         };
     }
 
-    selectLocation(location: Location | null) {
+    selectLocation(location: Location | null, center: boolean) {
         if (location) {
-            setTimeout(() => { google.maps.event.trigger(this.locationsToMarkers.get(location), "click"); }, 0);
+            setTimeout(() => {
+                const marker = this.locationsToMarkers.get(location)!;
+                this.map.setCenter(marker.getPosition());
+                google.maps.event.trigger(marker, "click");
+            }, 0);
         } else {
             this.infoWindow.close();
         }

angular/src/app/i18n.service.ts
@@ -439,6 +439,10 @@ const i18n = {
             en: "Show location pictures",
             de: "Fotos der Locations anzeigen",
         },
+        clickInListCentersLocation: {
+            en: "Click on location list centers location",
+            de: "Klick auf Location in der Liste zentriert die Location",
+        },
         map: {
             header: {
                 en: "Map",

angular/src/app/main/main.component.html
@@ -45,7 +45,8 @@
             <app-results-list [hidden]="isInitializing"
                 [locations]="filteredLocations"
                 [coordinates]="query?.distance?.place?.coordinates"
-                (locationSelect)="onLocationSelectInResultsList($event)"
+                (locationSelect)="onLocationSelectInResultsList($event, false)"
+                (locationCenter)="onLocationSelectInResultsList($event, true)"
             ></app-results-list>
         </div>
         <div class="col-right" [ngClass]="{fullMapView: fullMapView}">

angular/src/app/main/main.component.ts
@@ -132,11 +132,11 @@ export class MainComponent implements OnInit {
                 .sort(this.getSortFunction());
     }
 
-    onLocationSelectInResultsList(location: Location) {
+    onLocationSelectInResultsList(location: Location, center: boolean) {
         if (this.hasMobileSize) {
             this.enableFullMapView();
         }
-        this.googleMapComponent.selectLocation(location);
+        this.googleMapComponent.selectLocation(location, center);
         this.selectedLocation = location;
     }
 

angular/src/app/model/settings.ts
@@ -1,6 +1,7 @@
 export class Settings {
     rememberLastQuery = true;
     showPictures = true;
+    clickInListCentersLocation = true;
     map = {
         mapTypeControl: true,
         zoomControl: false,

angular/src/app/results-list/results-list.component.html
@@ -6,7 +6,7 @@
                     title="{{i18n.showOnMap}}"
                     href=""
                     role="button"
-                    (click)="$event.preventDefault(); locationSelect.emit(location)"
+                    (click)="$event.preventDefault(); onLocationClick(location)"
                 >{{location.name}}</a>
             </h1>
             <div>
@@ -15,7 +15,7 @@
                     src="{{configurationService.getIconUrl(location.veganCategory)}}"
                     style="height: 25px; width: auto; position:relative; bottom: -6px;"
                     role="button"
-                    (click)="locationSelect.emit(location)"
+                    (click)="onLocationClick(location)"
                     alt=""
                 />
             </div>

angular/src/app/results-list/results-list.component.ts
@@ -4,6 +4,7 @@ import { DomUtil } from "@marco-eckstein/js-utils";
 import { ConfigurationService } from "../configuration.service";
 import { I18nService } from "../i18n.service";
 import { Location } from "../model/location";
+import { LocalStorageService } from "../local-storage.service";
 
 @Component({
     selector: "app-results-list",
@@ -14,12 +15,14 @@ export class ResultsListComponent implements OnInit {
 
     constructor(
         readonly configurationService: ConfigurationService,
+        private readonly localStorageService: LocalStorageService,
         private readonly i18nService: I18nService,
     ) { }
 
     @Input() locations: Location[];
     @Input() coordinates: Coordinates | null;
     @Output() readonly locationSelect = new EventEmitter<Location>();
+    @Output() readonly locationCenter = new EventEmitter<Location>();
 
     readonly i18n = this.i18nService.getI18n();
     readonly expandOpenComments = new Map<Location, boolean>();
@@ -35,6 +38,14 @@ export class ResultsListComponent implements OnInit {
 
         if (!DomUtil.isElementVisible(locationElement)) {
             locationElement.scrollIntoView();
+        }
+    }
+
+    onLocationClick(location: Location) {
+        if (this.localStorageService.settings.clickInListCentersLocation) {
+            this.locationCenter.emit(location);
+        } else {
+            this.locationSelect.emit(location);
         }
     }
 }

angular/src/app/settings/settings.component.html
@@ -28,6 +28,15 @@
                 />{{i18n.settings.showPictures}}
             </label>
             <br/>
+            <label>
+                <input
+                    type="checkbox"
+                    name="clickInListCentersLocation"
+                    [(ngModel)]="localStorageService.settings.clickInListCentersLocation"
+                    (change)="onQueryChange()"
+                />{{i18n.settings.clickInListCentersLocation}}
+            </label>
+            <br/>
             <fieldset>
                 <legend>{{i18n.settings.map.header}}</legend>
                 <label>

commit a0c56d05a6d4237d08d46bd4f40ac9c790ab6fc9

Bug fix (was introduced in commit 951eeb9ece83840f093c7da64b82b5d8057ce460)

angular/src/app/google-map/google-map.component.ts
@@ -137,11 +137,13 @@ export class GoogleMapComponent implements OnInit {
         };
     }
 
-    selectLocation(location: Location | null, center: boolean) {
+    selectLocation(location: Location | null, center = false) {
         if (location) {
             setTimeout(() => {
                 const marker = this.locationsToMarkers.get(location)!;
-                this.map.setCenter(marker.getPosition());
+                if (center) {
+                    this.map.setCenter(marker.getPosition());
+                }
                 google.maps.event.trigger(marker, "click");
             }, 0);
         } else {