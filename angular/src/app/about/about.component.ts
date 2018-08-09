c1a6d01f486e9764e2abacd61ffc2d2225c90b81
Marco Eckstein <marco.eckstein@gmx.de>
2018-08-09 05:48:37 +0200

    Improve AboutComponent

--- angular/src/app/about/about.component.ts
+++ angular/src/app/about/about.component.ts
@@ -5,7 +5,8 @@ import { environment } from "../../environments/environment";
 
 @Component({
     selector: "app-about",
-    templateUrl: "about.component.html",
+    templateUrl: "./about.component.html",
+    styleUrls: [ "./about.component.scss" ],
 })
 export class AboutComponent {
 
@@ -14,4 +15,11 @@ export class AboutComponent {
     readonly i18n = this.i18nService.getI18n();
     readonly production = environment.production;
     readonly versionInfo = environment.versionInfo;
+
+    get gitHubCommitUrl(): string {
+        const vi = this.versionInfo;
+        const baseUrl = vi.repositoryUrl.substring(0, vi.repositoryUrl.length  - 4); // Remove ".git"
+        const sha1 = vi.gitDescription.hash.substring(1); // Remove "g"
+        return `${baseUrl}/commit/${sha1}`;
+    }
 }
