13780f186a99aca7c47eba0f70fd91d60cfdd3c3
Marco Eckstein <marco.eckstein@gmx.de>
2018-12-13 21:46:40 +0100

    Refactor TextClearComponent and use it in all browsers

--- angular/src/app/text-clear/text-clear.component.ts
+++ angular/src/app/text-clear/text-clear.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    ContentChild,
+    ElementRef,
+    Input,
+    ViewChild
+} from "@angular/core";
 
 // TODO: Move to library
 @Component({
@@ -6,7 +14,65 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
     templateUrl: "./text-clear.component.html",
     styleUrls: [ "./text-clear.component.scss" ],
 })
-export class TextClearComponent {
-    @Input() text: string;
-    @Output() readonly textChange = new EventEmitter<string>();
+export class TextClearComponent implements AfterViewInit {
+
+    constructor(private readonly changeDetectorRef: ChangeDetectorRef) {}
+
+    @Input() showButtonWhenOutside = false;
+    @ContentChild("input") input: ElementRef;
+    @ContentChild("buttonContent") buttonContent: ElementRef;
+    @ViewChild("button") button: ElementRef;
+    initialized = false;
+    focus = false;
+    hover = false;
+
+    get inputElement(): HTMLInputElement { return this.input.nativeElement; }
+
+    private get buttonElement(): HTMLButtonElement { return this.button.nativeElement; }
+
+    get showButton(): boolean {
+        return !this.initialized
+            || (!!this.inputElement.value && (this.showButtonWhenOutside || this.focus || this.hover));
+    }
+
+    ngAfterViewInit() {
+        if (!this.input) {
+            throw new Error("#input is mandatory content.");
+        }
+
+        const inputPaddingRightPx =
+            parseInt(window.getComputedStyle(this.inputElement).paddingRight!.replace("px", ""), 10);
+        const paddingRightEm = getEmValueFromElement(this.inputElement, inputPaddingRightPx);
+        const buttonWidthEm = getEmValueFromElement(this.buttonElement, this.buttonElement.offsetWidth);
+        this.buttonElement.style.right = paddingRightEm + "em";
+        this.inputElement.style.paddingRight = (2 * paddingRightEm + buttonWidthEm) + "em";
+        this.inputElement.style.flex = "1";
+        this.inputElement.addEventListener("focus", () => this.focus = true);
+        this.inputElement.addEventListener("blur", () => this.focus = false);
+
+        this.initialized = true;
+        this.changeDetectorRef.detectChanges(); // Otherwise ExpressionChangedAfterItHasBeenCheckedError
+    }
+
+    onMouseenter() { this.hover = true; }
+
+    onMouseleave() { this.hover = false; }
+
+    onClick() {
+        this.inputElement.value = "";
+        this.inputElement.dispatchEvent(new Event("input"));
+        this.inputElement.focus();
+    }
 }
+
+function getEmValueFromElement(element: HTMLElement, pixelValue: number): number {
+    if (!element.parentElement) {
+        throw new Error("Unexpected condition");
+    }
+    const parentFontSize = parseFloat(window.getComputedStyle(element.parentElement).fontSize!);
+    const elementFontSize = parseFloat(window.getComputedStyle(element).fontSize!);
+    const pixelValueOfOneEm = (elementFontSize / parentFontSize) * elementFontSize;
+    return pixelValue / pixelValueOfOneEm;
+}
+
+(window as any)["getEmValueFromElement"] = getEmValueFromElement;
