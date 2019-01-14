823393bd840ed8c1e0038987fc0f130bdd6723cd
Marco Eckstein <marco.eckstein@gmx.de>
2019-01-14 23:04:37 +0100

    refactor: remove unused code
    Forgotten in commit 91e636bc06516aa1406e64d035d25b517a383451

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,21 +1,8 @@
 import { Component } from "@angular/core";
-import { NavigationEnd, Router } from "@angular/router";
-import { filter, pairwise } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
-
-    static previousUrl = "";
-
-    constructor(router: Router) {
-        router.events.pipe(
-            filter(event => event instanceof NavigationEnd),
-            pairwise(),
-        ).subscribe((pair: any) => {
-            AppComponent.previousUrl = pair[0].url;
-        });
-    }
 }
