c9719de34a3bab5e0872ed86f0e7e23e54230646
Marco Eckstein <marco.eckstein@gmx.de>
2019-01-14 23:23:00 +0100

    feat: proper SPA tracking

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,8 +1,24 @@
 import { Component } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter, map } from "rxjs/operators";
+
+declare var gtag: any;
+const websiteGaTrackingId = "UA-1323925-1";
 
 @Component({
   selector: "app-root",
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
+
+    constructor(router: Router) {
+        router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            map(event => event as NavigationEnd)
+        ).subscribe(event => {
+            if (gtag) {
+                gtag("config", websiteGaTrackingId, { "page_path": event.urlAfterRedirects });
+            }
+        });
+    }
 }
